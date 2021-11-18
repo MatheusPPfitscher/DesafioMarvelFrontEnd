2a05f4f4dccf2f67480e21ef467450a8ca0f3ccc
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2021-11-18 18:24:45 -0300

    Adiciona função gerar tabela com imagens

--- public/api.js
+++ public/api.js
@@ -2,6 +2,10 @@ const api = axios.create({
     baseURL: "http://localhost:8081"
 });
 
+const limit = 50; //limite requisições
+
+const limitePaginas = 10;
+
 const params = new URLSearchParams(window.location.search);
 
 const page = params.get("page")
@@ -14,7 +18,8 @@ function listaPersonagens() {
     api.get("/", {
         params: {
             page,
-            name
+            name,
+            limit
         }
     })
         .then(result => {
@@ -30,16 +35,43 @@ function listaPersonagens() {
 }
 
 function atualizaTabela(lista) {
-    const tbodyLista = document
-        .querySelector("#listaPersonagens > tbody")
+    const tbodyLista = document.querySelector("#listaPersonagens > tbody")
     tbodyLista.innerHTML = "";
-    for (let personagem of lista) {
-        tbodyLista.innerHTML += `<tr>
-        <td>${personagem.id}</td>
-        <td>${personagem.name}</td>
-        <td> <img alt="${personagem.name}" height=60 src=${personagem.photo}
-        </tr>`
+    counter = 0;
+    console.log(lista);
+    let limRow = Math.floor(lista.length / 10)
+    console.log(limRow);
+    let conteudo = ""
+    for (let row = 0; row < limRow; row++) {
+        conteudo += "<tr>"
+        for (let colum = 0; colum < 10; colum++) {
+            personagem = lista[counter]
+            conteudo += `<td><img alt="${personagem.name}" height=60 src=${personagem.photo}></td>`
+            counter++;
+        }
+        conteudo += "</tr>"
+    }
+    tbodyLista.innerHTML = conteudo;
+}
+
+function atualizaPaginacao(page) {
+    listaPaginas = document.querySelector("#paginacao")
+    if (page <= (limitePaginas / 2)) {
+        for (let pagina = 1; pagina <= limitePaginas; pagina++) {
+            listaPaginas.innerHTML += `<li class="page-item"><a class="page-link" href="?page=${pagina}">${pagina}</a></li>`
+        }
+    }
+    else {
+        let limite = (parseInt(page) + limitePaginas / 2 + 1);
+        for (let pagina = (parseInt(page) - (limitePaginas / 2 - 1)); (pagina < limite); pagina++) {
+            listaPaginas.innerHTML += `<li class="page-item"><a class="page-link" href="?page=${pagina}">${pagina}</a></li>`
+        }
     }
 }
+
+function iniciarPagina() {
+    atualizaPaginacao(page)
+    listaPersonagens()
+}
 // http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg
-listaPersonagens();
+iniciarPagina()
