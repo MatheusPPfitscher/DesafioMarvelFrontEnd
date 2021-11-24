1269dafc526121f234db79f9c2273710124b8b76
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2021-11-23 21:06:14 -0300

    ajustes heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+const port = process.env.PORT || 8082;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname + "/../public"));
 
-app.listen(8082, () => console.log("Server is running..."));
+app.listen(port, () => console.log("Server is running..."));
